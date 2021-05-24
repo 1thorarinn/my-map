1ef2756062abcc4fa87ae3d07472482e0fd3d9e4
drullan <drullan.dev@gmail.com>
2021-05-25 01:45:15 +0200

    - Route creation

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React, { memo, useRef, useEffect, useState } from 'react';
 import { Select } from '@buffetjs/core';
 import Modal from 'react-modal';
 import MapboxGLGeocoder from '@mapbox/mapbox-gl-geocoder';
-import MapboxGLDraw from "@mapbox/mapbox-gl-draw";
+
 import MapboxGL from 'mapbox-gl';
+
+import { setStorage, getStorage, getRouteLine, Draw, customStyles, messages, checkFeaturesAmount, postData } from './map-utils.js';
 
 import 'mapbox-gl/dist/mapbox-gl.css';
 import 'mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css'
@@ -22,35 +24,9 @@ const mapTile = 'mapbox://styles/mapbox/streets-v11'
 const placesLimit = 6;
 const user_id = 12;
 
-const messages = {
-  chooseRoute : 'Choose a route...'
-}
-
-const customStyles = {
-  content : {
-    //width                 : '45%',
-    top                   : '50%',
-    left                  : '50%',
-    right                 : 'auto',
-    bottom                : 'auto',
-    marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)'
-  }
-}
-
-
-
-const Draw = new MapboxGLDraw({
-  displayControlsDefault: true,
-  controls: {
-    polygon: true,
-    polyline: true,
-    marker: true,
-    trash: true
-  }
-});
-
 const HomePage = () => {
+  
+  var subtitle, subtitle2;
 
   // Map Settings
   const map = useRef(null);
@@ -65,19 +41,16 @@ const HomePage = () => {
   // NEW Route settings
   const [routeName, setRouteName] = useState('')
   const [routeDescription, setRouteDescription] = useState('')
-  const [routeGeoData, setRouteGeoData] = useState([])
+  const [routeGeoData, setRouteGeoData] = useState(getStorage('currentMap') ?? {features:{}})
   const [modalStatus, setModalStatus] = useState(false);  
   const [clientRoutes, setClientRoutes] = useState([]);
   
-  const [newRouteId, setNewRouteId] = useState('0')
+  const [newRouteId, setNewRouteId] = useState(0)
 
   const [selectedRoute, setSelectedRoute] = useState(getStorage('currentMap') ?? [])
 
-  var subtitle, subtitle2;
+  
 
-  function afterOpenModal() {
-    subtitle.style.color = '#000';
-  }
 
   if (typeof(window) !== 'undefined') {
     Modal.setAppElement('body')
@@ -108,7 +81,9 @@ const HomePage = () => {
       },
       style: mapTile,
       center: [ lng, lat ],
-      zoom: zoom
+      zoom: zoom,
+      minZoom: 4,
+      maxZoom: 18
     });
 
     map.current.on('load', function () {      
@@ -128,10 +103,11 @@ const HomePage = () => {
         setStorage('currentMapOptions',{
           lat: map.current.getCenter().lat,
           lng: map.current.getCenter().lng,
-          zoom: map.current.getZoom()
+          zoom: Math.round(map.current.getZoom())
         })
       });
 
+      map.current.on('draw.add', createDrawArea);
       map.current.on('draw.create', createDrawArea);
       map.current.on('draw.update', updateDrawArea);
       map.current.on('draw.delete', deleteDrawArea);
@@ -146,9 +122,9 @@ const HomePage = () => {
 
     });
 
-    const createDrawArea = (e) => { setStoredMap(e, Draw.getAll()) }
-    const updateDrawArea = (e) => { setStoredMap(e, Draw.getAll()) }    
-    const deleteDrawArea = (e) => { setStoredMap(e, Draw.getAll()) }   
+    const createDrawArea = (e) => { setStoredMap(e.type, Draw.getAll()) }
+    const updateDrawArea = (e) => { setStoredMap(e.type, Draw.getAll()) }    
+    const deleteDrawArea = (e) => { setStoredMap(e.type, Draw.getAll()) }   
 
   }
 
@@ -157,46 +133,19 @@ const HomePage = () => {
     setRouteGeoData(data)
   }
 
-  function setStorage(key, data, type='json'){
-    return ( type === 'json')
-      ? localStorage.setItem(key, JSON.stringify(data))
-      : localStorage.setItem(key, data)
-  }
-
-  function getStorage(key, type='json'){
-    return (type === 'json')
-      ? JSON.parse(localStorage.getItem(key))
-      : localStorage.getItem(key);
-  }
-
   function launchToast(message, doContinue=false){
     alert(message);
     closeModal()
     return doContinue
   }
 
-  // Checking stored LiveMap
-  function checkFeaturesAmount(data=[], type='Point'){
-    let amount = 0
-    for(var i=0; i < data.features.length; i++){
-      if( data.features[i].geometry.type === type){
-        amount++
-      } 
-    }
-    return amount
-  }
-
-  function getRouteLine(data=[], type='Point'){
-    for(var i=0; i < data.features.length; i++){
-      if( data.features[i].geometry.type === type){
-        return data
-      } 
-    }
-  }
-
-
+  // Modal control
   function openModal() {
-    if(!routeGeoData.features) return false
+
+    console.log(routeGeoData)
+
+    if(routeGeoData === []) return false
+
     // Analizing the data to save :)
     if(routeGeoData.features.length > 0){
 
@@ -204,7 +153,7 @@ const HomePage = () => {
 
         let storedLinesAmount = checkFeaturesAmount(routeGeoData, 'LineString');
         if(storedLinesAmount > 1 ){
-          if(!launchToast('You are unable to draw two lines for a route. You must delete '+(storedLinesAmount-1)+' lines')) return false
+          if(!launchToast('You are unable to draw two lines for a route. \n\n You must delete '+(storedLinesAmount-1)+' lines\n')) return false
         }else if(storedLinesAmount === 0){
           if(!launchToast('You must draw one route, at least')) return false
         }          
@@ -213,18 +162,19 @@ const HomePage = () => {
 
         let storedPointsAmount = checkFeaturesAmount(routeGeoData, 'Point');
         if(storedPointsAmount < 1 ){
-          if (!window.confirm("You don't have addressed any Place to your route. We encourage you to put, at least, one Place Marker ;)' Do you really want to save?")) {
-            if(!launchToast('You cancel to save this Route')) return false
+          if (!window.confirm("You don't have addressed any Place to your route. \nWe encourage you to put, at least, one Place Marker ;)\n\n Do you really want to save?\n\n")) {
+            if(!launchToast('You canceled to save this Route')) return false
           }
         }else if(storedPointsAmount > placesLimit ){
           if(!launchToast('You have passed the amount limit of Places on your route. Please, the limit are '+placesLimit)) return false
         }
 
-    }else if(!launchToast("You haven't created a route. Please, print at least a Route!")){
-
-    }else{
-      setModalStatus(true);
+    }else{ 
+      return launchToast("You haven't created a route. Please, print at least a Route!")
     }
+
+    console.log('Opening Modal To Save ^^')
+    setModalStatus(true);
 
   }
 
@@ -232,29 +182,8 @@ const HomePage = () => {
     setModalStatus(false);
   }
 
-  async function postData(url = '', data = {}) {
-    // Opciones por defecto estan marcadas con un *
-    const response = await fetch(url, {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      mode: 'cors', // no-cors, *cors, same-origin
-      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: 'same-origin', // include, *same-origin, omit
-      headers: { 'Content-Type': 'application/json'
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      redirect: 'follow', // manual, *follow, error
-      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: JSON.stringify(data) // body data type must match "Content-Type" header
-    }).catch(function(error) {
-      console.log(error);
-    });
-    return response.json(); // parses JSON response into native JavaScript objects
-  }
-
-  function viewRouteData(){
-    console.log('Route name: '+routeName)
-    console.log('Route description: '+routeDescription)
-    console.log(routeGeoData)
+  function afterOpenModal() {
+    subtitle.style.color = '#000';
   }
 
   function getSaveRouteModal(){
@@ -297,23 +226,29 @@ const HomePage = () => {
     </Modal>
   }
 
-  function saveRoute(){      
-
+  function viewRouteData(){
     console.log('Saving the GeoJSON map data!!!')
     console.log('Route name: '+routeName)
     console.log('Route description: '+routeDescription)
     console.log(routeGeoData)
-
-    subtitle2.style.color = 'green';
-    launchToast("Everything is ok. We gonna save the route. Please wait...")
-
-    return processSave()
-
   }
 
-  function postRoute(){
+  function saveRoute(){
+    viewRouteData()
+    subtitle2.style.color = 'green';
+    //launchToast("Everything is ok. We gonna save the route. Please wait...")
+    return processSave()
+  }
 
-    let postRouteData = {
+  function processSave(){
+    setNewPostRoute()   
+    closeModal()
+    return true;
+  }
+
+  function setNewPostRoute(){
+
+    let setNewPostRouteData = {
       "name": routeName,
       "creator": user_id,
       "description": [
@@ -331,20 +266,29 @@ const HomePage = () => {
       }
     }
     
-    postData(host+'/routes', postRouteData )
+    postData(host+'/routes', setNewPostRouteData )
     .then(data => {
       console.log('Route was posted successful ;)')
-      setNewRouteId(data.id)
+      if(data.id !== '0'){
+        setNewRouteId(data.id)
+        for(var i=0; i < routeGeoData.features.length; i++){
+          if(routeGeoData.features[i].geometry.type === 'Point'){
+            setNewPostPlace(data.id, routeGeoData.features[i].id, routeGeoData, i)
+          }else if(routeGeoData.features[i].geometry.type === 'Polygon'){
+            setNewPostPolygon(data.id, routeGeoData.features[i].id, routeGeoData, i)
+          }
+        }
+      }
     });
 
   }
 
-  function setNewPostPlace(key, placeFeatures, markerType=3, i){
+  function setNewPostPlace(routeId, key, placeFeatures, markerType=3, i){
 
     let setNewPostPlaceData = {
-      "name": routeName+' Place '+i,
+      "name": routeName+' Place '+routeId+'-'+i,
       "creator": user_id,
-      "parent_route" : newRouteId,
+      "parent_route" : routeId,
       "description": [
         {
           "language": 1,
@@ -367,12 +311,12 @@ const HomePage = () => {
 
   }
 
-  function setNewPostPolygon(key, polygonFeatures, i){
+  function setNewPostPolygon(routeId, key, polygonFeatures, i){
 
-    let postRouteData = {
-      "name": routeName+' Warning Polygon '+i,
+    let setNewPostRouteData = {
+      "name": routeName+' Warning '+routeId+'-'+i,
       "creator": user_id,
-      "parent_route" : newRouteId,
+      "parent_route" : routeId,
       "map_data": {
         "id": "string",
         "center_lat": lat,
@@ -382,30 +326,11 @@ const HomePage = () => {
       },
     }
     
-    postData(host+'/polygons', postRouteData )
+    postData(host+'/polygons', setNewPostRouteData )
     .then(data => {
       console.log('Polygon '+key+' posted successful ;)')
     });
 
-  }
-
-  function processSave(){
-    postRoute()
-    /*if(newRouteId){
-      for(var i=0; i < routeGeoData.features.length; i++){
-        if(routeGeoData.features[i].geometry.type === 'Point'){
-          setNewPostPlace(routeGeoData.features[i].id, routeGeoData, i)
-        }else if(routeGeoData.features[i].geometry.type === 'Polygon'){
-          setNewPostPolygon(routeGeoData.features[i].id, routeGeoData, i)
-        }
-      }
-    }*/
-    let endData = []
-    setStorage('currentMap', endData)
-    setRouteGeoData(endData)
-    loadMap()
-    closeModal()
-    return true;
   }
 
   const [clientPlaces, setClientPlaces] = useState([]);
---
+++ src/map-utils.js
@@ -0,0 +1,77 @@
+import MapboxGLDraw from "@mapbox/mapbox-gl-draw";
+
+export function setStorage(key, data, type='json'){
+    return ( type === 'json')
+        ? localStorage.setItem(key, JSON.stringify(data))
+        : localStorage.setItem(key, data)
+}
+
+export function getStorage(key, type='json'){
+    return (type === 'json')
+        ? JSON.parse(localStorage.getItem(key))
+        : localStorage.getItem(key);
+}
+
+export function getRouteLine(data=[], type='Point'){
+    for(var i=0; i < data.features.length; i++){
+        if( data.features[i].geometry.type === type){
+        return data
+        } 
+    }
+}
+
+export const Draw = new MapboxGLDraw({
+    displayControlsDefault: true,
+    controls: {
+        polygon: true,
+        polyline: true,
+        marker: true,
+        trash: true
+    }
+});
+  
+export const customStyles = {
+    content : {
+        //width                 : '45%',
+        top                   : '50%',
+        left                  : '50%',
+        right                 : 'auto',
+        bottom                : 'auto',
+        marginRight           : '-50%',
+        transform             : 'translate(-50%, -50%)'
+    }
+}
+
+export const messages = {
+    chooseRoute : 'Choose a route...'
+}
+
+// Checking stored LiveMap
+export function checkFeaturesAmount(data=[], type='Point'){
+    let amount = 0
+    for(var i=0; i < data.features.length; i++){
+        if( data.features[i].geometry.type === type){
+            amount++
+        } 
+    }
+    return amount
+}
+
+export async function postData(url = '', data = {}) {
+    // Opciones por defecto estan marcadas con un *
+    const response = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: { 'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
+    }).catch(function(error) {
+      console.log(error);
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+  }

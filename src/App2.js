8889829b3bcaf4eec89ef0dbbc93c1ab45817263
drullan <drullan.dev@gmail.com>
2021-05-27 02:12:37 +0200

    - Design improvements - Other workarounds

--- src/App2.js
+++ src/App2.js
@@ -2,24 +2,14 @@ import React, { memo, useRef, useEffect, useState } from 'react';
 import { Select } from '@buffetjs/core';
 import Modal from 'react-modal';
 import MapboxGLGeocoder from '@mapbox/mapbox-gl-geocoder';
-import RouteSelector from './RouteSelector.js'
 
 import MapboxGL from 'mapbox-gl';
-
-// BuffetJS
-import { Button, InputText } from '@buffetjs/core';
-import { LoadingBar  } from '@buffetjs/styles';
-
-// Fontawsome...
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 import { setStorage, getStorage, getRouteType, Draw, customStyles, messages, removeStorage, checkFeaturesAmount, postData } from './map-utils.js';
 
 import 'mapbox-gl/dist/mapbox-gl.css';
 import 'mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css'
 import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 
 const host ='http://161.97.167.92:1337';
@@ -41,33 +31,35 @@ const defaultCenter = {
 
 const HomePage = () => {
   
+  var subtitle, subtitle2;
+
   const [userId, setUserId] = useState(12)
 
   // Map Settings
   const map = useRef(null);
   const mapContainer = useRef(null);
-  
-  // Client routes for the select button
-  const [clientRoutes, setClientRoutes] = useState([]);
+  const currentMapOpts = getStorage('currentMapOptions') ?? defaultCenter;
 
-  // Current Map
-  const [mapMode, setMapMode] = useState(getStorage('mapMode', 'string') ?? 'creation')
-  const currentMapOpts = getStorage('currentRouteOptions') ?? defaultCenter;
   const [focusLat,  setMapLat]  = useState(currentMapOpts.lat);
   const [focusLng,  setMapLng]  = useState(currentMapOpts.lng);
   const [focusZoom, setMapZoom] = useState(currentMapOpts.zoom);
 
+  // Loading all the CMS client routes....
+  const [clientRoutes, setClientRoutes] = useState([]);
+  const [mapMode, setMapMode] = useState('creation')
 
-  // Related with routes...
-  const [currentRoute, setCurrentRoute] = useState(getStorage('currentRoute') ?? { features: {} })
-  const [currentRouteId, setCurrentRouteId] = useState(getStorage('currentRouteId') ?? 0)
+  const [newRouteId, setNewRouteId] = useState(0)
+  const [currentRoute, setCurrentRoute] = useState([]);
+  const [routeGeoData, setRouteGeoData] = useState(getStorage('currentMap') ?? {features:{}})
 
-  // NEW Route data
-  const [modalStatus, setModalStatus] = useState(false);
-  const [routeName, setRouteName] = useState(getStorage('routeName', 'string') ?? '')
-  const [routeDescription, setRouteDescription] = useState(getStorage('routeDescription', 'string') ?? '')
-  const [saveButtonDisabled, setSaveButtonDisabled] = useState(getStorage('disabledMainSaveButton', 'string') === 'false' ? false : true)
-  const [saveButtonSuccess, setSaveButtonSuccess] = useState(getStorage('saveButtonSuccess', 'string') ?? 'primary')
+  const [currentMap, setSelectedRoute] = useState(getStorage('currentMap') ?? [])
+
+    // NEW Route settings
+  const [routeName, setRouteName] = useState('')
+  const [routeDescription, setRouteDescription] = useState('')
+
+  const [modalStatus, setModalStatus] = useState(false);  
+  //const [sessionTimestamp, setSessionTimestamp] = useState(Date.now())
 
   if (typeof(window) !== 'undefined') {
     Modal.setAppElement('body')
@@ -83,7 +75,7 @@ const HomePage = () => {
     loadMap()
   },[]);
 
-  function loadMap(){
+  function loadMap(){    
     if (map.current) return;
     map.current = new MapboxGL.Map({
       container: mapContainer.current,
@@ -111,77 +103,70 @@ const HomePage = () => {
         marker: false
       }));      
 
-      map.current.on('move', () => { setMapOptions() });
+      map.current.on('move', () => { memoMapData() });
 
-      map.current.on('draw.add', updateDrawArea);
-      map.current.on('draw.create', updateDrawArea);
+      map.current.on('draw.add', createDrawArea);
+      map.current.on('draw.create', createDrawArea);
       map.current.on('draw.update', updateDrawArea);
-      map.current.on('draw.delete', updateDrawArea);
+      map.current.on('draw.delete', deleteDrawArea);
       map.current.addControl(new MapboxLanguage());
       //map.current.addControl(new MapboxGL.FullscreenControl());
       //map.current.addControl(new MapboxGL.GeolocateControl({ positionOptions: { enableHighAccuracy: true }, trackUserLocation: false }));
 
       // Draw temporary stored map!!
-      if(getStorage('currentRoute')){
-        Draw.add(getStorage('currentRoute'));
+      if(getStorage('currentMap')){
+        Draw.add(getStorage('currentMap'));
       }
 
     });
 
-    //const addDrawArea = (e) => { setStoredMap(e, Draw.getAll()) }
-    //const createDrawArea = (e) => { setStoredMap(e, Draw.getAll()) }
-    const updateDrawArea = (e) => { setStoredMap(e, Draw.getAll()) }    
-    //const deleteDrawArea = (e) => { setStoredMap(e, Draw.getAll()) }   
-
-  }
-
-  function resetMap(){
-    removeStorage('currentRoute')
-    Draw.deleteAll()
-  }
-
-  function mainSaveButtonStatus(status){
-    setStorage('disabledMainSaveButton', status, 'string')
-    setSaveButtonDisabled(status)
-  }
-    
-  function setStoredMap(type, data){
-
-    mainSaveButtonStatus(false)
-    setStorage('currentRoute', data)
-    setCurrentRoute(data)
-    console.log(type, [data,mapMode])
-    switch(mapMode){
-      case 'creation':
-
-
-        break;
-      default:
-      case 'edition':
-
-        break;
-    }
+    const createDrawArea = (e) => { setStoredMap(e.type, Draw.getAll()) }
+    const updateDrawArea = (e) => { setStoredMap(e.type, Draw.getAll()) }    
+    const deleteDrawArea = (e) => { setStoredMap(e.type, Draw.getAll()) }   
 
   }
   
+  function setStoredMap(type, data){
+    console.log('storedMap::'+mapMode)
+    if(mapMode === 'creation'){
+      setStorage('currentMap', data)
+      setRouteGeoData(data)
+    }else if(mapMode === 'edition'){
+
+
+
+
+      console.log(type, data)
+
+
+
+
+
+    }
+  }
+
+  function resetMap(){
+    removeStorage('currentMap')
+    Draw.deleteAll()
+  }
+
   function launchToast(message, doContinue=false){
     alert(message);
+    closeModal()
     return doContinue
   }
 
   // Modal control
-  function validateSaving() {
+  function openModal() {
 
-    console.log(currentRoute)
-
-    if(currentRoute === []) return false
+    if(routeGeoData === []) return false
 
     // Analizing the data to save :)
-    if(currentRoute.features.length > 0){
+    if(routeGeoData.features.length > 0){
 
       // 1.- Only a Linestring is allowed!! Not two      
 
-        let storedLinesAmount = checkFeaturesAmount(currentRoute, 'LineString');
+        let storedLinesAmount = checkFeaturesAmount(routeGeoData, 'LineString');
         if(storedLinesAmount > 1 ){
           if(!launchToast('You are unable to draw two lines for a route. \n\n You must delete '+(storedLinesAmount-1)+' lines\n')) return false
         }else if(storedLinesAmount === 0){
@@ -190,7 +175,7 @@ const HomePage = () => {
 
       // 2.- Now you will set the first location (meeting-point)
 
-        let storedPointsAmount = checkFeaturesAmount(currentRoute, 'Point');
+        let storedPointsAmount = checkFeaturesAmount(routeGeoData, 'Point');
         if(storedPointsAmount < 1 ){
           if (!window.confirm("You don't have addressed any Place to your route. \nWe encourage you to put, at least, one Place Marker ;)\n\n Do you really want to save?\n\n")) {
             if(!launchToast('You canceled to save this Route')) return false
@@ -203,40 +188,79 @@ const HomePage = () => {
       return launchToast("You haven't created a route. Please, print at least a Route!")
     }
 
-    saveRoute()
+    console.log('Opening Modal To Save ^^')
+    setModalStatus(true);
 
   }
 
-  function storeRouteName(name){
-    setRouteName(name)
-    setStorage('routeName', name, 'string')
-    setSaveButtonSuccess('primary')
-    mainSaveButtonStatus(false)
+  function closeModal(){
+    setModalStatus(false);
+  }
+
+  function afterOpenModal() {
+    subtitle.style.color = '#000';
+  }
+
+  function getSaveRouteModal(){
+    return <Modal
+      isOpen={modalStatus}
+      onAfterOpen={afterOpenModal}
+      onRequestClose={closeModal}
+      style={customStyles}
+      contentLabel="Save your route"
+    >
+      <h2 ref={_subtitle => (subtitle = _subtitle)}>Save your route</h2>
+      <label ref={_subtitle2 => (subtitle2 = _subtitle2)}>Please, include the basic info...</label>
+      <div className='table'>
+        <div className='row'>
+          <div className='col-12'>
+            <label>Route Name</label>
+          </div>
+          <div className='col-12'>
+            <input type='text' id='route-name' onChange={e=>{setRouteName(e.target.value)}}/>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col-12'>
+          <label>Route Description</label>
+          </div>
+          <div className='col-12'>
+          <textarea id='route-description' onChange={e=>{setRouteDescription(e.target.value)}}/>
+          </div>
+        </div>
+
+        <div className='row'>
+          <div className='col-6'>
+            <button onClick={closeModal}>Cancel</button>
+          </div>
+          <div className='col-6'>
+            <button onClick={saveRoute}>Save</button>
+          </div>
+        </div>        
+      </div>
+    </Modal>
   }
 
   function viewRouteData(){
     console.log('Saving the GeoJSON map data!!!')
     console.log('Route name: '+routeName)
     console.log('Route description: '+routeDescription)
-    console.log(currentRoute)
+    console.log(routeGeoData)
   }
 
   function saveRoute(){
     viewRouteData()
+    subtitle2.style.color = 'green';
     //launchToast("Everything is ok. We gonna save the route. Please wait...")
-    return processRouteSave()
+    return processSave()
   }
 
-  function processRouteSave(){
-
-    switch(mapMode){
-      case 'creation':
-        setNewPostRoute()   
-        break;
-      default:
-      case 'edition':
-        alert('Please, wait the updating feature!! ^__^!')
-        break;
+  function processSave(){
+    if(mapMode === 'creation'){
+      setNewPostRoute()   
+      closeModal()
+    }else if(mapMode === 'edition'){ 
+      alert('Please, wait the updating feature!! ^__^!')
     }
 
     return true;
@@ -257,29 +281,23 @@ const HomePage = () => {
         "center_lat": focusLat,
         "center_long": focusLng,
         "center_zoom": focusZoom,
-        "data" : getRouteType(currentRoute, 'LineString')
+        "data" : getRouteType(routeGeoData, 'LineString')
       }
     } )
     .then(data => {
-
+      console.log('Route '+data.id+' was posted successful ;)')
       if(data.id !== '0'){
+        for(var i=0; i < routeGeoData.features.length; i++){
 
-        for(var i=0; i < currentRoute.features.length; i++){
+          if(routeGeoData.features[i].geometry.type === 'Point'){
+            setNewPostPlace(data.id, routeGeoData.features[i].id, routeGeoData.features[i],3, i)
 
-          if(currentRoute.features[i].geometry.type === 'Point'){
-            setNewPostPlace(data.id, currentRoute.features[i].id, currentRoute.features[i],3, i)
-
-          }else if(currentRoute.features[i].geometry.type === 'Polygon'){
-            setNewPostPolygon(data.id, currentRoute.features[i].id, currentRoute.features[i], i)
+          }else if(routeGeoData.features[i].geometry.type === 'Polygon'){
+            setNewPostPolygon(data.id, routeGeoData.features[i].id, routeGeoData.features[i], i)
 
           }
         }
-
       }
-
-      setSaveButtonSuccess('success')
-      console.log('Route '+data.id+' was posted successful ;)')
-
     });
   }
 
@@ -336,43 +354,34 @@ const HomePage = () => {
     
   }
 
-  function setRoute(route_id){
+  function setRoute(value){
 
     resetMap()
 
-    fetch(routesOrigin+'/'+route_id)
+    fetch(routesOrigin+'/'+value)
       .then((res) => res.json())
       .catch(error => console.error('Error:', error))
       .then(response => {
 
-        setCurrentRouteId(route_id);
-        setStorage('currentRouteId', route_id)        
+        setSelectedRoute(value);
 
         if(!response){
 
           console.log('CREATION MODE!')
           setMapMode('creation')
-          setStorage('mapMode', 'creation', 'string')
-
-          setRouteName('')
 
         }else{
 
-          setRouteName(response.name)
-
           console.log('EDITION MODE!')
           setMapMode('edition')
-          setStorage('mapMode', 'edition', 'string')
 
           let selected = {
             type: 'FeatureCollection', features: []
           }
 
-          response.map_data.data.id = response.id
+          response.map_data.id = response.id
           Draw.add(response.map_data.data)
           selected.features.push(response.map_data.data)
-
-          console.log(response)
 
           if(response.places !== undefined){
             for(var i = 0; i < response.places.length; i++){
@@ -388,11 +397,11 @@ const HomePage = () => {
               let data = response.polygons[z].map_data.data
               data.id = response.polygons[z].id
               Draw.add(data)
-              selected.features.push(data)
+              selected.features.push({data})
             }
           }
 
-          setStorage('currentRoute', selected)
+          setStorage('currentMap', selected)
 
           map.current.flyTo({
             center:[
@@ -411,24 +420,26 @@ const HomePage = () => {
   }
 
   function renderRoutesSelector(routes){
-    var options = [{ value: '0', color: 'grey', label: messages.chooseRoute }]
+    var options = [{ value: '0', label: messages.chooseRoute }]
     for(var i = 0; i < clientRoutes.length; i++){
       options.push({ value: clientRoutes[i].id.toString(), label: routes[i].name })
     }
     return <Select
       name="route"
-      value={currentRouteId}
+      value={currentMap}
       options={options}
       closeMenuOnSelect={true}
-      onChange={({ target: { value } }) => { setRoute(value) }}
+      onChange={({ target: { value } }) => {
+        setRoute(value)
+      }}
     />
   }
 
-  function setMapOptions(){
+  function memoMapData(){
     setMapLat(map.current.getCenter().lat);
     setMapLng(map.current.getCenter().lng);        
     setMapZoom(Math.round(map.current.getZoom()));
-    setStorage('currentRouteOptions',{
+    setStorage('currentMapOptions',{
       lat: map.current.getCenter().lat,
       lng: map.current.getCenter().lng,
       zoom: Math.round(map.current.getZoom())
@@ -438,43 +449,22 @@ const HomePage = () => {
   return (
     <>
       <div className="row">
-        <div className="col-lg-8 col-md-8">
+        <div className="col-lg-8 col-md-12">
             <div>
               <div>
+                {/*<div className="sidebar">Longitude: {lng.toFixed(4)} | Latitude: {lat.toFixed(4)} | Zoom: {Math.round(zoom)}</div>*/}
+                {renderRoutesSelector(clientRoutes)}
+                <button color="success" disabled="" onClick={openModal}>Save route</button>
+                <div className="calculation-box">                  
+                  <div id="calculated-area"></div>
+                </div>
                 <div ref={mapContainer} className="map-container" />
-                <div className="nav-bar">Longitude: {focusLng.toFixed(4)} | Latitude: {focusLat.toFixed(4)} | Zoom: {Math.round(focusZoom)}</div>
               </div>
             </div>
         </div>
-        <div className="col-md-4 col-lg-4">
-          <br/>
-          <div className='row'>
-            <div className='col-6'>
-            </div>
-            <div className='col-6'>
-              <Button color={saveButtonSuccess} disabled={saveButtonDisabled} onClick={validateSaving} icon={<FontAwesomeIcon icon={faPlus} />} label="Save" />
-            </div>
-          </div> 
-          <br/>
-          <div class='row'>
-            {renderRoutesSelector(clientRoutes)}
-            <RouteSelector/>
-          </div>
-          <br/>
-          <div className='row'>
-            <label>Route name</label>
-          </div>
-          <div className='row'>
-            <InputText
-              type='text'
-              name='route-name'
-              value={routeName} 
-              placeholder='Set the route name...'
-              onChange={({ target: { value } }) =>{storeRouteName(value)}}
-            />
-          </div>
-          {/*getSaveRouteModal()*/}                 
-          {/*<button onClick={viewStored('currentRouteData')}>[View storage]</button>*/}          
+        <div className="col-md-12 col-lg-4">
+          {getSaveRouteModal()}       
+          {/*<button onClick={viewStored('currentMapData')}>[View storage]</button>*/}          
         </div>
       </div>
     </>
---
+++ src/RouteSelector.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RouteSelector = () => {
+
+    return <></>
+
+}
+
+export default RouteSelector
